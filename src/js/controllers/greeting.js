7e5188ba975d463c2bf85e070574cc17d7f523f2
Brit Butler <redline6561@gmail.com>
2017-03-13 11:29:22 -0400

    Do proper dependency injection so that if we minify the source things still work.

--- src/js/controllers/greeting.js
+++ src/js/controllers/greeting.js
@@ -1,5 +1,8 @@
 function GreetingController ($scope) {
   console.log("Welcome to GreetingController");
+  console.log($scope);
 }
+
+GreetingController.$inject = ['$scope'];
 
 export default GreetingController;
