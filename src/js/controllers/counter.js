283b31a326910d971f2f0b4b40b48a8474fc8767
Brit Butler <redline6561@gmail.com>
2017-03-13 10:31:10 -0400

    Add a click handler to our page.

--- src/js/controllers/counter.js
+++ src/js/controllers/counter.js
@@ -1,5 +1,9 @@
 function CounterController ($scope) {
   console.log("Welcome to CounterController");
+
+  $scope.sayHi = function () {
+    console.log("cool");
+  };
 }
 
 export default CounterController;
